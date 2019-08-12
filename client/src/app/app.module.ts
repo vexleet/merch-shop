2d9e0c032f23738282ae460a99ea41b7e8cbfd54
vexleet <vex_malamov@abv.bg>
2019-08-12 19:47:22 +0300

    Add nav, home, footer component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
