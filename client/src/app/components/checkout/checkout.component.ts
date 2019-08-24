4e0bfbfd862b2a631e821314dc9a82391dcf3375
vexleet <vex_malamov@abv.bg>
2019-08-24 19:09:28 +0300

    add full form of checkout

--- client/src/app/components/checkout/checkout.component.ts
+++ client/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,10 @@
+import { CartService } from './../../core/services/cart.service';
+import { CheckoutService } from './../../core/services/checkout.service';
 import { FormBuilder } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { ICartProduct } from 'src/app/core/models';
+
+declare let paypal;
 
 @Component({
   selector: 'app-checkout',
@@ -7,6 +12,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
+  @ViewChild('creditCard', { static: true }) creditCardElement: ElementRef;
+
   informationForm = this.fb.group({
     fullName: [''],
     email: [''],
@@ -17,10 +25,50 @@ export class CheckoutComponent implements OnInit {
     postalCode: [''],
   });
 
+  paymentForm = this.fb.group({
+    cardNumber: [''],
+    expDate: [''],
+    securityCode: ['']
+  });
+
+
+  cartProducts: Array<ICartProduct>;
+
   constructor(
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private renderer: Renderer2,
+    private checkoutService: CheckoutService,
+    private cartService: CartService) { }
 
   ngOnInit() {
+    this.cartProducts = JSON.parse(this.cartService.getCartProducts());
+
+    paypal.Buttons({
+      createOrder: (data, actions) => {
+        return new Promise(resolve => {
+          this.checkoutService.checkOutPaypal(this.cartProducts)
+            .subscribe((res) => {
+              resolve(res["orderID"]);
+            });
+        });
+      },
+
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log(order);
+      }
+    }).render(this.paypalElement.nativeElement);
+  }
+
+  changePayment(value: string) {
+    if (value === 'credit') {
+      this.renderer.setStyle(this.creditCardElement.nativeElement, "display", "block");
+      this.renderer.setStyle(this.paypalElement.nativeElement, "display", "none");
+    }
+    else if (value === 'paypal') {
+      this.renderer.setStyle(this.creditCardElement.nativeElement, "display", "none");
+      this.renderer.setStyle(this.paypalElement.nativeElement, "display", "block");
+    }
   }
 
 }
